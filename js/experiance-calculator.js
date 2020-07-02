e043c731819696345dba287e5af086cfc4a027c5
isajjadali <sajjad.ali5112@gmail.com>
2020-07-02 12:42:45 +0500

    experiance issue resolved

--- js/experiance-calculator.js
+++ js/experiance-calculator.js
@@ -1,9 +1,14 @@
-function getExperiance(date, remainingMonths) {
-    let years = new Date().getFullYear() - new Date(date).getFullYear();
-    let otherMonths = new Date().getMonth() + 1 + remainingMonths;
+function getExperience(date) {
+    let years = new Date().getFullYear() - new Date(date).getFullYear() - 1;
+
+    let otherMonths = new Date().getMonth() + 1 + (12 - (new Date(date).getMonth() + 1));
+
     if (otherMonths < 12) {
-        document.write(years + '.' + otherMonths);
+        document.write(years + ((otherMonths + '').length < 2 ? '.0' : '.') + otherMonths);
+        return;
+    } else if (otherMonths === 12) {
+        document.write(years + 1 + '.00');
         return;
     }
-    document.write((years + (otherMonths / 12)).toFixed(2));
+    document.write((years + 1 + '.0' + Math.round(otherMonths / 12)));
 }
